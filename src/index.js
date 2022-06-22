24f2261760bcae4f61d048e553f9e4f2639c8237
Александр Сдобников <sdobnikove@mail.ru>
2022-06-23 02:27:01 +0300

    test

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './styles.scss';
 
 const button = document.querySelector('button');
 
-button.addEventListener('click', 'e' => {
+button.addEventListener('click', (e) => {
     e.preventDefault();
     console.log('hi!');
 });
