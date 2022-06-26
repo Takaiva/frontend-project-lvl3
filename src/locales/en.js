9f8479b80420ea152c966390ae117b70c75d5aad
Александр Сдобников <sdobnikove@mail.ru>
2022-06-26 22:24:32 +0300

    add locales

---
+++ src/locales/en.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    submitButton: 'Submit',
+    exampleLink: 'Example',
+    form: {
+      feedbackStatus: {
+        success: 'RSS successfully uploaded',
+        failure: {
+          notOneOf: 'Provided RSS already exists',
+          validationError: 'Invalid URL',
+        },
+      },
+    },
+  },
+};
---
+++ src/locales/ru.js
@@ -0,0 +1,15 @@
+export default {
+  translation: {
+    submitButton: 'Добавить',
+    exampleLink: 'Пример',
+    form: {
+      feedbackStatus: {
+        success: 'RSS успешно загружен',
+        failure: {
+          notOneOf: 'RSS уже существует',
+          validationError: 'Ссылка должна быть валидным URL',
+        },
+      },
+    },
+  },
+};
