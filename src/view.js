a43cd1fb1fe7dc74c397a52f31ad4953292015b0
Александр Сдобников <sdobnikove@mail.ru>
2022-07-04 03:26:43 +0300

    add interface disabling while setting up rss, render for containers for feend and posts, render for feed/posts items

--- src/view.js
+++ src/view.js
@@ -1,36 +1,75 @@
-import onChange from "on-change";
 import i18next from "i18next";
 import resources from './locales/index.js';
+import renderPostsAndFeedsContainers from "./renders/renderPostsFeedsContainers.js";
+import renderFeed from "./renders/renderFeed.js";
+import renderPosts from "./renders/renderPosts.js";
 
-export default (state) => {
-  const i18n = i18next.createInstance();
-  i18n.init({
-    lng: 'ru',
+export default (elements) => (path, value, previousValue) => {
+  const defaultLanguage = 'en';
+  const translation = i18next.createInstance();
+  translation.init({
+    lng: defaultLanguage,
     debug: false,
     resources,
   }).then((t) => { t('key'); });
 
-  const feedbackEl = document.querySelector('p.feedback');
-  const inputEl = document.querySelector('input');
+  const {
+    feedback, input, fieldset, feedsContainer, postsContainer,
+  } = elements;
 
-  return onChange(state, (path, value, previousValue) => {
-    switch (path) {
-      case 'form.feedbackStatus':
-        feedbackEl.textContent = i18n.t(`${path}.${value}`);
-        break;
-      case 'form.isValidForm':
-        const actualHighlight = value === true ? `text-success` : `text-danger`;
-        const previousHighlight = previousValue === true ? 'text-success' : 'text-danger';
-        feedbackEl.classList.remove(previousHighlight);
-        feedbackEl.classList.add(actualHighlight);
-        if (value) {
-          inputEl.classList.remove('is-invalid');
-        } else {
-          inputEl.classList.add('is-invalid');
-        }
-        break;
-      default:
-        break;
-    }
-  });
+  switch (path) {
+    case 'feedFetchingProcess':
+      if (value === 'started') {
+        fieldset.setAttribute('disabled', '');
+      }
+      if (value === 'finished') {
+        fieldset.removeAttribute('disabled');
+      }
+      break;
+    case 'postsAndFeedsContainersState':
+      if (value === 'render') {
+        // when containers for feed and post items are rendered
+        renderPostsAndFeedsContainers(feedsContainer, postsContainer);
+      }
+      break;
+    case 'feeds':
+      // render last added feed item
+      const feedItemsContainer = document.querySelector('div.feeds ul.list-group');
+      const lastAddedFeedItem = (value[value.length - 1]);
+      const { feedTitle, feedDescription } = lastAddedFeedItem;
+      const renderedFeedElement = renderFeed(feedTitle, feedDescription);
+      feedItemsContainer.prepend(renderedFeedElement);
+      break;
+    case 'posts':
+      // render last added post items
+      const postItemsContainer = document.querySelector('div.posts ul.list-group');
+      const previouslyAddedPostItems = previousValue;
+      const lengthOfPreviouslyAddedPostItems = previouslyAddedPostItems.length;
+      const allCurrentAddedPostItems = value;
+      const lastAddedPostItems = allCurrentAddedPostItems.slice(lengthOfPreviouslyAddedPostItems);
+      const renderedPostElements = lastAddedPostItems.map(({
+        postTitle, postLink, postId, feedId,
+      }) => renderPosts(postTitle, postLink, postId, feedId)).reverse();
+      renderedPostElements.forEach((el) => postItemsContainer.prepend(el));
+      break;
+    case 'form.feedbackStatus':
+      // render feedback status text
+      feedback.textContent = translation.t(`${path}.${value}`);
+      break;
+    case 'form.isValidForm':
+      // change feedback highlighting color
+      const actualHighlight = value === true ? `text-success` : `text-danger`;
+      const previousHighlight = previousValue === true ? 'text-success' : 'text-danger';
+      feedback.classList.remove(previousHighlight);
+      feedback.classList.add(actualHighlight);
+      // change input field border color
+      if (value) {
+        input.classList.remove('is-invalid');
+      } else {
+        input.classList.add('is-invalid');
+      }
+      break;
+    default:
+      break;
+  }
 };
