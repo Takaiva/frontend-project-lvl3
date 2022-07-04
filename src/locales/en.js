16e330d28038cfce36b856b2f21bc333fed931cb
Александр Сдобников <sdobnikove@mail.ru>
2022-07-04 03:25:14 +0300

    update texts for translation

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,13 +1,21 @@
 export default {
   translation: {
-    submitButton: 'Submit',
-    exampleLink: 'Example',
+    interface: {
+      submitButton: 'Submit',
+      feeds: 'Feeds',
+      posts: 'Posts',
+      preview: 'Preview',
+      exampleLink: 'Example: https://habr.com/ru/rss/hubs/all/',
+    },
     form: {
       feedbackStatus: {
         success: 'RSS successfully uploaded',
         failure: {
           notOneOf: 'Provided RSS already exists',
           validationError: 'Invalid URL',
+          parsingError: 'Provided resource does not contain valid RSS',
+          badResponse: 'Server returned error, most likely the session has expired, please refresh the page and try again',
+          noResponse: 'Bad connection or server is not responding',
         },
       },
     },
--- src/locales/ru.js
+++ src/locales/ru.js
@@ -1,13 +1,21 @@
 export default {
   translation: {
-    submitButton: 'Добавить',
-    exampleLink: 'Пример',
+    interface: {
+      submitButton: 'Добавить',
+      feeds: 'Фиды',
+      posts: 'Посты',
+      preview: 'Просмотр',
+      exampleLink: 'Пример: https://habr.com/ru/rss/hubs/all/',
+    },
     form: {
       feedbackStatus: {
         success: 'RSS успешно загружен',
         failure: {
           notOneOf: 'RSS уже существует',
           validationError: 'Ссылка должна быть валидным URL',
+          parsingError: 'Ресурс не содержит валидный RSS',
+          badResponse: 'Сервер вернул ошибку, скорее всего сессия истекла, обновите страницу и повторите попытку',
+          noResponse: 'Плохое соединение или сервер не отвечает',
         },
       },
     },
