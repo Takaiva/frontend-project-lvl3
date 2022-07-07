300ce22dffd5442099ce8cbc9419cd76b7f90b01
Александр Сдобников <sdobnikove@mail.ru>
2022-07-07 23:20:16 +0300

    update making post ids ordered and set correct feed ids

--- src/updatePosts.js
+++ src/updatePosts.js
@@ -1,22 +1,32 @@
-import downloadRss from './RSSdownloader.js';
-import parseRss from './RSSparser.js';
 import _ from 'lodash';
+import downloadRss from './RSSdownloader.js';
+import parseForUpdating from "./RSSparserForUpdating";
+import pkg from 'lodash';
 
-export default (urls, state) => {
-    urls.forEach((url) => {
-        downloadRss(url)
-            .then((response) => {
-                parseRss(response.data.contents)
-                    .then(({ posts }) => {
-                        const updatedPosts = posts;
-                        const oldPostTitles = state.posts.map((post) => post.postTitle)
-                        const allNewPostTitles = updatedPosts.map((post) => post.postTitle);
-                        const newPostsTitles = _.differenceWith(allNewPostTitles, oldPostTitles, _.isEqual);
-                        newPostsTitles.forEach((title) => {
-                            const newPost = updatedPosts.filter((post) => post.postTitle === title);
-                            state.posts = state.posts.concat(newPost);
-                        });
-                    });
+const { uniqueId } = pkg;
+//To make post ids ordered, i am using another parsing function,
+//to make correct feedId relation between posts and feeds, and
+//to trigger uniqueId() only on updated post items.
+//Also to not to collide with parsing function (from RSSparser.js),
+//that triggers on submit event
+export default (state) => {
+  state.feeds.forEach((feed) => {
+    downloadRss(feed.feedOriginLink)
+      .then((response) => {
+        parseForUpdating(response.data.contents)
+          .then(({ posts }) => {
+            const updatedPosts = posts;
+            const oldPostTitles = state.posts.map((post) => post.postTitle);
+            const allNewPostTitles = updatedPosts.map((post) => post.postTitle);
+            const newPostsTitles = _.differenceWith(allNewPostTitles, oldPostTitles, _.isEqual);
+            newPostsTitles.forEach((title) => {
+              const newPost = updatedPosts.find((post) => post.postTitle === title);
+              console.log(newPost);
+              newPost.feedId = feed.feedId;
+              newPost.postId = Number(uniqueId());
+              state.posts = (state.posts).concat([newPost]);
             });
-    });
+          });
+      });
+  });
 };
