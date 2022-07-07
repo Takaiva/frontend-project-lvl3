f2394e6abf0b08f090bd332caa74a80b72af3264
Александр Сдобников <sdobnikove@mail.ru>
2022-07-07 23:17:26 +0300

    add event listeners for modal window and translation buttons

--- src/app.js
+++ src/app.js
@@ -8,30 +8,33 @@ import updatePosts from './updatePosts.js';
 
 const app = () => {
   const elements = {
-    form: document.querySelector('form'),
+    formEl: document.querySelector('form'),
     fieldset: document.querySelector('form fieldset'),
     input: document.querySelector('form input'),
     button: document.querySelector('form button'),
     feedback: document.querySelector('p.feedback'),
     feedsContainer: document.querySelector('div.feeds'),
     postsContainer: document.querySelector('div.posts'),
+    modalWindow: document.getElementById('modal'),
+    translationButtons: document.querySelectorAll('.translation'),
   };
 
   const state = onChange({
     currentLng: 'en', // en, ru
-    urls: [], // rss-stream urls
     feeds: [],
     posts: [],
-    feedFetchingProcess: '', // started, finished
+    updatingProcess: null, // started, null
+    feedFetchingProcess: null, // started, finished
     postsAndFeedsContainersState: 'not rendered', // not rendered, render
     form: {
-      feedbackStatus: '', // success/failure.{error}
-      isValidForm: '', // true/false
+      feedbackStatus: null, // success/failure.{error}
+      isValidForm: null, // true/false
     },
+    modalWindowObject: null,
   }, render(elements));
 
   const validateLink = (link) => {
-    const links = state.urls;
+    const links = state.feeds.map(feed => feed.feedOriginLink);
     yup.setLocale({
       mixed: {
         notOneOf: 'notOneOf',
@@ -46,7 +49,7 @@ const app = () => {
 
   const runPostUpdatingProcess = () => {
     const period = 5000;
-    updatePosts(state.urls, state);
+    updatePosts(state);
     setTimeout(() => runPostUpdatingProcess(), period);
   };
 
@@ -55,9 +58,9 @@ const app = () => {
     state.feedFetchingProcess = 'started';
     validateLink(url).then((validatedUrl) => {
       downloadRss(validatedUrl).then((response) => {
-        parseRss(response.data.contents).then(({ feed, posts }) => {
-          console.log(feed);
-          // when parsing finished successfully, rendering containers for feed and post items
+        parseRss(response.data.contents, url, state).then(({ feed, posts }) => {
+          // when parsing finished successfully, if needed containers are not rendered,
+          // render containers for feed and post items
           if (state.postsAndFeedsContainersState === 'not rendered') {
             state.postsAndFeedsContainersState = 'render';
           }
@@ -66,28 +69,36 @@ const app = () => {
           state.posts = (state.posts).concat(posts);
           state.feeds = (state.feeds).concat(feed);
 
-          // performing feedback status
+          // display success message
           state.form.feedbackStatus = 'success';
 
           // enable form interface when the rss setting up is finished
           state.feedFetchingProcess = 'finished';
 
-          // update info about added urls
-          state.urls.push(url);
-
           // update feedback highlighting color (red or green) depending on the feedback status
           state.form.isValidForm = !(state.form.feedbackStatus).includes('failure');
           // console.log(posts);
           // reset form when rss is downloaded and set up successfully
-          elements.form.reset();
-          runPostUpdatingProcess();
+          elements.formEl.reset();
+
+          // user-friendly practice
+          elements.input.focus();
+
+          // start updating posts process
+          //using state to not to trigger updating more that one times
+          if (state.updatingProcess !== 'started') {
+            runPostUpdatingProcess();
+            state.updatingProcess = 'started';
+          }
         }).catch((parsingError) => {
+          // display error message
           const errorMessage = parsingError.message;
           state.form.feedbackStatus = `failure.${errorMessage}`;
           state.form.isValidForm = !(state.form.feedbackStatus).includes('failure');
           state.feedFetchingProcess = 'finished';
         });
       }).catch((networkError) => {
+        // display error message
         if (networkError.response) {
           state.form.feedbackStatus = `failure.badResponse`;
         }
@@ -98,6 +109,7 @@ const app = () => {
         state.feedFetchingProcess = 'finished';
       });
     }).catch((validationError) => {
+      // display error message
       const errorMessage = validationError.message;
       state.form.feedbackStatus = `failure.${errorMessage}`;
       state.form.isValidForm = !(state.form.feedbackStatus).includes('failure');
@@ -105,12 +117,27 @@ const app = () => {
     });
   };
 
-  elements.form.addEventListener('submit', (e) => {
+  elements.formEl.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(e.target);
     const url = formData.get('url');
     startRssSetup(url);
   });
+
+  elements.modalWindow.addEventListener('show.bs.modal', (e) => {
+    const button = e.relatedTarget;
+    const neededPostId = Number(button.dataset.id);
+    const posts = state.posts.map(post => post);
+    const neededPost = posts.find(post => post.postId === neededPostId);
+    state.modalWindowObject = neededPost;
+  });
+
+  // translate interface into ru or en
+  elements.translationButtons.forEach((button) => button.addEventListener('click', (e) => {
+    const targetButton = e.target;
+    const language = targetButton.dataset.lang;
+    state.currentLng = language;
+  }))
 };
 
 export default app;
