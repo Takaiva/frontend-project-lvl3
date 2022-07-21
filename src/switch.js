c56eb42393bb5efb161254f075f8d713b1c7cf5b
Александр Сдобников <sdobnikove@mail.ru>
2022-07-21 05:14:56 +0300

    add function making feeds switchable

---
+++ src/switch.js
@@ -0,0 +1,24 @@
+import _ from 'lodash';
+
+export default (state, feedElement) => {
+  feedElement.addEventListener('click', (e) => {
+    const liEl = e.target.closest('li');
+    liEl.classList.toggle('border-0');
+    liEl.classList.toggle('border-end-0');
+    liEl.classList.toggle('border');
+    liEl.classList.toggle('border-success');
+    const title = liEl.querySelector('h3');
+    const titleText = title.textContent;
+    const correspondingFeed = _.cloneDeep(state.feeds.find((feedItem) => feedItem.feedTitle === titleText));
+    const id = correspondingFeed.feedId;
+    state.posts.forEach((post) => {
+      if (post.feedId === id) {
+        post.show = !post.show;
+      }
+    });
+    const isAnyActiveFeed = state.posts.some((post) => post.show === id);
+    if (!isAnyActiveFeed) {
+      state.posts.forEach((post) => post.show === null);
+    }
+  });
+};
